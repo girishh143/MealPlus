bb559ea221830f80a0608d588fb6578bf8cc3d28
unknown <deepakg@ULTP_374.synerzipune.local>
2018-07-05 19:42:29 +0530

    Changes made in AddStudent001

--- src/test/java/TestCases/AddStudent001.java
+++ src/test/java/TestCases/AddStudent001.java
@@ -3,41 +3,24 @@ package TestCases;
 import K12PaymentCenter.BaseTest;
 import K12PaymentCenter.pages.DashboardPage;
 import K12PaymentCenter.pages.SettingsPage;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class AddStudent001 extends BaseTest {
+    
     @Test(description = "Verify the User can add a verified Student.", groups = {"AddStudent"}, priority = 0)
     public void testAddVerifiedStudent() throws InterruptedException {
-        Thread.sleep(5000);
 
         Login();
-        Thread.sleep(5000);
-
-        Login();
-
-        Thread.sleep(10000);
 
         DashboardPage dashboardPage = new DashboardPage(driver);
+
         dashboardPage.getSettingsButton().click();
-
-        Thread.sleep(3000);
-
-
-
-
-
 
         SettingsPage settingsPage = new SettingsPage(driver);
         settingsPage.getLogoutButton().click();
     }
-
-
-
-
-
-
-
-
 
 
 }
