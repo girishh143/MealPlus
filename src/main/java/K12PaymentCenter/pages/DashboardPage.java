b597209e24b357cba78fe331aa3c677af6384737
unknown <deepakg@ULTP_374.synerzipune.local>
2018-07-10 21:36:44 +0530

    added two files MealPayment.java and MealPayment.yml

--- src/main/java/K12PaymentCenter/pages/DashboardPage.java
+++ src/main/java/K12PaymentCenter/pages/DashboardPage.java
@@ -14,11 +14,7 @@ public class DashboardPage {
     protected Map<String, String> xpaths;
     protected AndroidDriver driver;
     private MobileElement settingsButton;
-    private MobileElement addButton;
-    private MobileElement addAmountTextbox;
-    private MobileElement addToCartButton;
-    private MobileElement addPaymentSuccessMessage;
-    private MobileElement addPaymentOkButton;
+
 
     public DashboardPage(AndroidDriver driver) {
         this.driver = driver;
@@ -42,32 +38,5 @@ public class DashboardPage {
         settingsButton = (MobileElement) (driver.findElements(By.className("android.widget.ImageView"))).get(2);
         return settingsButton;
     }
-    
-    public MobileElement getAddButton(){
-        WebDriverWait wait = new WebDriverWait(this.driver, 90);
-        addButton = DriverHelper.getMobileElement(this.xpaths.get("BY_XPATH_ADD_BUTTON"), this.driver);
-        wait.until(ExpectedConditions.visibilityOf(addButton));
-        return addButton;
-    }
 
-    public MobileElement getAddAmountTextbox(){
-        addAmountTextbox=DriverHelper.getMobileElement(this.xpaths.get("BY_XPATH_ADDAMOUNT_TEXTBOX"),this.driver);
-        return addAmountTextbox;
-
-    }
-
-    public MobileElement getAddToCartButton(){
-        addToCartButton=DriverHelper.getMobileElement(this.xpaths.get("BY_XPATH_ADDTOCART_BUTTON"),this.driver);
-        return addToCartButton;
-    }
-
-    public MobileElement getAddPaymentSuccessMessage(){
-        addPaymentSuccessMessage=DriverHelper.getMobileElement(this.xpaths.get("BY_XPATH_ADDPAYMENT_SUCCESSMESSAGE"),this.driver);
-        return addPaymentSuccessMessage;
-    }
-
-    public MobileElement getAddPaymentOkButton(){
-        addPaymentOkButton=DriverHelper.getMobileElement(this.xpaths.get("BY_XPATH_ADDPAYMENT_SUCCESSOKBUTTON"),this.driver);
-        return addPaymentOkButton;
-    }
 }
