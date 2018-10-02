1faafde7c96405ed201d188f1628c267bd072c61
Girish Kore <girish.kore@synerzip.com>
2018-10-02 17:54:08 +0530

    Done chagnes in syso statement..git add .!

--- src/test/java/K12PaymentCenter/BaseTest.java
+++ src/test/java/K12PaymentCenter/BaseTest.java
@@ -34,6 +34,7 @@ public class BaseTest {
 
         driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), androidCapabilities);
         driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+
         return driver;
     }
 
